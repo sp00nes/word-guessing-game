e4a62a7f80cef8851cfe97ce088a5a41275dea4a
MARTIN HANSEN <marrtt1234@gmail.com>
2023-03-08 07:55:02 +0000

    basic game running

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
+// ---------------LAZY MANS CHEAT-SHEET ------------------------------------ //
+// socket.broadcast.emit : if i need to broadcast to namespace except sender.
+// socket.emit : sends back to sender
+// io.of("myNamespace").emit : all clients in namespace
+// io.in("room1").emit : all clients in room one
+// io.of("myNamespace").to("room1").emit : all clients in room one in namespace
+// ------------------------------------------------------------------------- //
+
+
 const { Server } = require('socket.io');
 const chalk = require('chalk');
 
@@ -21,6 +30,25 @@ game.on('connection', (socket)=>{
     } else {
       throw new Error(chalk.red('No Role'));
     }
+  });
+
+  socket.on('', () => {
+    console.log('Hello World');
+  });
+
+  socket.on('CLUE', (payload) => {
+    console.log('PAYLOAD CLUE:', payload);
+    socket.broadcast.emit('CLUE', payload);
+  });
+
+  socket.on('CORRECT-GUESS', (payload) => {
+    console.log('PAYLOAD CORRECT-GUESS:', payload);
+    socket.broadcast.emit('CORRECT-GUESS', payload);
+  });
+
+  socket.on('WRONG-GUESS', (payload) => {
+    console.log('PAYLOAD WRONG-GUESS:', payload);
+    socket.broadcast.emit('WRONG-GUESS', payload);
   });
 
 });
